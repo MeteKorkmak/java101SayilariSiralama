2e64dc57b836426c2b0eb75bef396dd9979b1fef
Mete Korkmak <mete.korkmak.34@gmail.com>
2022-03-27 17:59:02 +0300

    Sayıları Sıralama

---
+++ src/sayilariSıralama.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class sayilariSıralama {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int a,b,c;
+
+        System.out.print("1.sayıyı giriniz:");
+        a=input.nextInt();
+        System.out.print("2.sayıyı giriniz:");
+        b=input.nextInt();
+        System.out.print("3.sayıyı giriniz:");
+        c=input.nextInt();
+
+        if((a<b) && (a<c)){
+            if (b<c){
+                System.out.println( a + " < " + b + " < " + c);
+                System.out.println("a < b < c");
+            }else{
+                System.out.println( a + " < " + c + " < " + b);
+                System.out.println("a < c < b");
+            }
+        }else if((b<a) && (b<c)){
+            if (a<c){
+                System.out.println( b + " < " + a + " < " + c);
+                System.out.println("b < a < c");
+            }else{
+                System.out.println( b + " < " + c + " < " + a );
+                System.out.println("b < c < a");
+            }
+        }else {
+            if (a < b) {
+                System.out.println( c + " < " + a + " < " + b);
+                System.out.println("c < a < b");
+            } else {
+                System.out.println( c + " < " + b + " < " + a);
+                System.out.println("c < b < a");
+            }
+
+
+        }
+    }
+}
